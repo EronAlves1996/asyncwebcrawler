7fa4dbecf5d1ba5406b62deaea8800588ba2e336
EronAlves1996 <eron.alves@rocketmail.com>
2024-02-27 00:04:12 -0300

    feat: create method that returns completable future

--- src/main/java/com/eronalves/App.java
+++ src/main/java/com/eronalves/App.java
@@ -1,13 +1,18 @@
 package com.eronalves;
 
+import java.net.URL;
+import java.util.concurrent.CompletableFuture;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+  public static CompletableFuture<String> crawl(URL url) {
+    return CompletableFuture.completedFuture("");
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
 }
